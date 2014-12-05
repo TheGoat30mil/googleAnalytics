cf2066a0b36d0195df6602f99e4cea89c3b60162
TheGoat30mil <sbradley@devmac.local>
2014-12-05 15:27:19 -0500

    App is active now.
    App is showing activity in Google Analytics

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -8,18 +8,10 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.network-information/www/network.js",
-        "id": "org.apache.cordova.network-information.network",
-        "clobbers": [
-            "navigator.connection",
-            "navigator.network.connection"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
-        "id": "org.apache.cordova.network-information.Connection",
-        "clobbers": [
-            "Connection"
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
         ]
     },
     {
@@ -34,7 +26,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.2.12",
-    "org.apache.cordova.network-information": "0.2.13",
+    "org.apache.cordova.dialogs": "0.2.10",
     "org.apache.cordova.statusbar": "0.1.8"
 }
 // BOTTOM OF METADATA
